ef7b9381c813212eb6afbd6ec84b2c67c04ae022
gearson <deniz.dohmen@gmail.com>
2019-08-21 11:04:47 +0200

    added print to pybite_128

--- days/01-03-datetimes/code/pybite_128.py
+++ days/01-03-datetimes/code/pybite_128.py
@@ -13,12 +13,12 @@ def years_ago(date):
        it from the THIS_YEAR constant above, returning the int difference.
        So in this example you would get: 2018 - 2015 = 3"""
 #%%     
-
+print(years_ago('8 Aug, 2015'))
 
 def convert_eu_to_us_date(date):
     eu_date = datetime.strptime(date, '%d/%m/%Y')
     us_date = eu_date.strftime('%m/%d/%Y')
     return us_date
 
-
+print(convert_eu_to_us_date('17/01/1990'))
 #%%
